ce994e598bbf0484276b36527dd014b153e50b1d
Christopher Laeri <claeri27@gmail.com>
2019-07-05 19:46:32 -0400

    Fixed clock formatting

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,27 +4,26 @@ import styled from 'styled-components';
 const BodyContainer = styled.div`
 `;
 
-const TimeContainer = styled.div`
+const TimeAndDateContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
+  justify-content: center;
+  font-size: 2em;
 `;
 
-const DayContainer = styled.div`
+const TimeContainer = styled.div`
 `;
 
 const DateContainer = styled.div`
 `;
-
-const NumberContainer = styled.div`
-`
 
 const FormattedDate = () => { 
   const rawDate = new Date()
   const [hours, setHours] = useState(rawDate.getHours());
   const [minutes, setMinutes] = useState(rawDate.getMinutes());
   const [seconds, setSeconds] = useState(rawDate.getSeconds());
-  const [month, setMonth] = useState(rawDate.getMonth() + 1);
+  const [month, setMonth] = useState(rawDate.getMonth());
   const [day, setDay] = useState(rawDate.getDay());
   const [year, setYear] = useState(rawDate.getFullYear())
   const [weekDay, setWeekDay] = useState(rawDate.getDay());
@@ -34,7 +33,7 @@ const FormattedDate = () => {
       setHours(new Date().getHours())
       setMinutes(new Date().getMinutes())
       setSeconds(new Date().getSeconds())
-      setMonth(new Date().getMonth() + 1)
+      setMonth(new Date().getMonth())
       setDay(new Date().getDay())
       setYear(new Date().getFullYear())
       setWeekDay(new Date().getDay())
@@ -47,36 +46,43 @@ const FormattedDate = () => {
 
   const dayOfTheWeek = () => {
     switch(weekDay) {
-      case 0:
-        return 'Sunday'
-      case 1:
-        return 'Monday' 
-      case 2:
-        return 'Tuesday'
-      case 3:
-        return 'Wednesday'
-      case 4:
-        return 'Thursday'
-      case 5:
-        return 'Friday'
-      case 6:
-        return 'Saturday'
-      default:
-        return 'ERROR'
+      case 0: return 'Sunday'
+      case 1: return 'Monday' 
+      case 2: return 'Tuesday'
+      case 3: return 'Wednesday'
+      case 4: return 'Thursday'
+      case 5: return 'Friday'
+      case 6: return 'Saturday'
+      default: return 'ERROR'
     }
   }
 
-  return <TimeContainer>
-    <NumberContainer>
+  const monthString = () => {
+    switch(month) {
+      case 0: return 'January'
+      case 1: return 'February'
+      case 2: return 'March'
+      case 3: return 'April'
+      case 4: return 'May'
+      case 5: return 'June'
+      case 6: return 'July'
+      case 7: return 'August'
+      case 8: return 'September'
+      case 9: return 'October'
+      case 10: return 'November'
+      case 11: return 'December'
+      default: return 'ERROR'
+    }
+  }
+
+  return <TimeAndDateContainer>
+    <TimeContainer>
       {hours > 12 ? hours - 12 : hours}:{minutes < 10 ? '0' + minutes : minutes}:{seconds < 10 ? '0' + seconds : seconds} {hours < 12 ? 'AM' : 'PM'}
-    </NumberContainer>
-    <DayContainer>
-      {dayOfTheWeek()}
-    </DayContainer>
+    </TimeContainer>
     <DateContainer>
-      {day}/{month}/{year}
+      {dayOfTheWeek()} {monthString()} {day}, {year}
     </DateContainer>
-  </TimeContainer>
+  </TimeAndDateContainer>
 }
 
 
