67964e92358726981b8bd267204af9698a4388a1
Christopher Laeri <claeri27@gmail.com>
2019-07-06 17:46:49 -0400

    Added moment, moment-tz, and intl clock times

--- src/components/InternationalClocks.js
+++ src/components/InternationalClocks.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
+import moment from 'moment';
+import 'moment-timezone'
 import AnalogClock, { Themes } from 'react-analog-clock'
 
 const IntlClocksContainer = styled.div`
@@ -49,9 +51,16 @@ const DelhiContainer = styled.div`
 `;
 
 const ClockData = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const City = styled.h2`
+`;
+
+const CityTime = styled.h3`
 `;
 
 const InternationalClocks = () => {
@@ -59,30 +68,45 @@ const InternationalClocks = () => {
     <NewYorkContainer>
       <ClockData>
         <City>New York</City>
+        <CityTime>
+          {moment.tz("America/New_York").format('LT')}
+        </CityTime>
       </ClockData>
       <AnalogClock theme={Themes.dark} width={300} gmtOffset={'-4'}/>
     </NewYorkContainer>
     <LondonContainer>
       <ClockData>
         <City>London</City>
+        <CityTime>
+          {moment.tz("Europe/London").format('LT')}
+        </CityTime>
       </ClockData>
       <AnalogClock theme={Themes.dark} width={300} gmtOffset={'+1'}/>
     </LondonContainer>
     <HongKongContainer>
       <ClockData>
         <City>Hong Kong</City>
+        <CityTime>
+          {moment.tz("Asia/Hong_Kong").format('LT')}
+        </CityTime>
       </ClockData>
       <AnalogClock theme={Themes.dark} width={300} gmtOffset={'+8'}/>
     </HongKongContainer>
     <TokyoContainer>
       <ClockData>
         <City>Tokyo</City>
+        <CityTime>
+          {moment.tz("Asia/Tokyo").format('LT')}
+        </CityTime>
       </ClockData>
       <AnalogClock theme={Themes.dark} width={300} gmtOffset={'+9'}/>
     </TokyoContainer>
     <DelhiContainer>
       <ClockData>
         <City>Delhi</City>
+        <CityTime>
+          {moment.tz("Asia/Delhi").format('LT')}
+        </CityTime>
       </ClockData>
       <AnalogClock theme={Themes.dark} width={300} gmtOffset={+'5.5'}/>
     </DelhiContainer>
